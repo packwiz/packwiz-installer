26b8e1de866ed028dce90cdd3640381130bfc854
comp500 <comp500@users.noreply.github.com>
2019-05-21 01:19:39 +0100

    Close input stream (ProgressMonitor) after copying

--- src/main/java/link/infra/packwiz/installer/Main.java
+++ src/main/java/link/infra/packwiz/installer/Main.java
@@ -44,6 +44,7 @@ public class Main {
 	// TODO: link these somehow so they're only defined once?
 	private static void addBootstrapOptions(Options options) {
 		options.addOption(null, "bootstrap-update-url", true, "Github API URL for checking for updates");
+		options.addOption(null, "bootstrap-update-token", true, "Github API Access Token, for private repositories");
 		options.addOption(null, "bootstrap-no-update", false, "Don't update packwiz-installer");
 		options.addOption(null, "bootstrap-main-jar", true, "Location of the packwiz-installer JAR file");
 		options.addOption("g", "no-gui", false, "Don't display a GUI to show update progress");
